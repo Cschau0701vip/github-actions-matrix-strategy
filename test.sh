178db47505e96d46c3c3f0f9f16f323483f8ea75
Chetan Chaudhari <31230509+Cschau0701vip@users.noreply.github.com>
2023-01-28 03:09:18 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+echo 'Start of the script!'
+if [[ $platform == 'windows-latest' ]]; 
+then
+  echo $platform
+  npm test --category=life --file_path='./output.md'
+else
+  echo $platform
+  npm test $category=life $file_path='./output.md'
+fi
+echo 'End of the script!'
