9fd35a8452a683e00ed4d8208f40e4d5328cc471
Chetan Chaudhari <31230509+Cschau0701vip@users.noreply.github.com>
2023-01-28 03:44:21 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@ echo 'Start of the script!'
 if [[ $platform == 'windows-latest' ]]; 
 then
   echo $platform
-  npm test --category=life --file_path='./output.md'
+  npm test $category=life $file_path='./output.md'
 else
   echo $platform
-  npm test $category=life $file_path='./output.md'
+  npm test --category=life --file_path='./output.md'
 fi
 echo 'End of the script!'
