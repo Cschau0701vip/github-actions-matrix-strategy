268ae3e990e73ba08415336b5122c410fe1b9f99
Chetan Chaudhari <31230509+Cschau0701vip@users.noreply.github.com>
2023-01-28 02:47:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 const axios = require("axios");
 
 const category = core.getInput('category') || 'inspire';
-const file_path = core.getInput('readme_path') || 'output.md';
+const file_path = core.getInput('file_path') || 'output.md';
 
 (async () => {
   try {
