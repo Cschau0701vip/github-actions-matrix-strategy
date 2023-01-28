91421a5d544a0e8a9f9d339e40e97f77d9b70647
Chetan Chaudhari <31230509+Cschau0701vip@users.noreply.github.com>
2023-01-28 13:11:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ console.log(file_path);
     console.log(data);
     let qotd = data.contents.quotes[0].quote;
     console.log(qotd);
-    quote = quote.concat(`💬 Quote of the Day: "${qotd}"\n`);
+    let quote = `💬 Quote of the Day: "${qotd}"\n`;
     console.log(quote);
     // Write file with new quote
     await fs.writeFile(file_path, quote, { signal });
