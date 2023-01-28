c69c80db46a7b21501f7863a6107f094963fcc9c
Chetan Chaudhari <31230509+Cschau0701vip@users.noreply.github.com>
2023-01-28 12:52:44 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,13 +3,13 @@ echo 'Start of the script!'
 if [ "$#" -ne 1 ]; then
     echo "No arguments supplied"
 fi
-
+echo "$#"
 if [[ $platform == 'windows-latest' ]]; 
 then
   echo $platform
-  npm test $category=life $file_path='./output.md'
+  npm test -- $category=life $file_path='./output.md'
 else
   echo $platform
-  npm test -category=life -file_path='./output.md'
+  npm test -- --category=life file_path:'./output.md'
 fi
 echo 'End of the script!'
